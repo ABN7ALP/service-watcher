560666da59c22f2a61e55b3af156f18dfa742d86
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2025-12-14 22:43:33 +0300

    Create Spin.js

---
+++ server/models/Spin.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const spinSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    cost: {
+        type: Number,
+        default: 1
+    },
+    status: {
+        type: String,
+        enum: ['win', 'lose'],
+        required: true
+    },
+    netResult: {
+        type: Number,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Spin', spinSchema);
