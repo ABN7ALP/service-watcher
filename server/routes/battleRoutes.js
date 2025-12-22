39ce64b2d5b529356a78ce963a83f07ffb8fec21
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2025-12-22 19:22:44 +0300

    Update battleRoutes.js

--- server/routes/battleRoutes.js
+++ server/routes/battleRoutes.js
@@ -11,7 +11,10 @@ router.route('/')
     .get(battleController.getAvailableBattles) // GET /api/battles
     .post(battleController.createBattle);      // POST /api/battles
 
+
 // (سيتم إضافة مسارات الانضمام والمغادرة هنا لاحقاً)
 // router.post('/:id/join', battleController.joinBattle);
+router.post('/:id/join', battleController.joinBattle); // POST /api/battles/some-battle-id/join
+
 
 module.exports = router;
