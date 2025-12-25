d1f838f6fa988ba438f07661a1e2c6c486762172
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2025-12-25 06:01:50 +0300

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware'); // ✅ تأكد من استيراد هذا
 
 const router = express.Router();
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+
+router.patch('/updateMyPassword', authMiddleware, authController.updatePassword);
 
 module.exports = router;
