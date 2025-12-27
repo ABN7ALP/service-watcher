791447024799184bc820aa12f1bb5a37e9a745f7
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2025-12-27 23:42:54 +0000

    Create friendRoutes.js

---
+++ server/routes/friendRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const friendController = require('../controllers/friendController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+// حماية جميع المسارات التالية
+router.use(authMiddleware);
+
+router.post('/send-request/:userId', friendController.sendFriendRequest);
+router.post('/accept-request/:userId', friendController.acceptFriendRequest);
+router.post('/reject-request/:userId', friendController.rejectOrCancelRequest);
+router.delete('/remove-friend/:userId', friendController.removeFriend);
+
+module.exports = router;
