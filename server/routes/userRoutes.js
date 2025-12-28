cfb416809a254a053abc419bdcf296e3b0738dfd
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2025-12-28 05:25:16 +0300

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,11 +5,17 @@ const userController = require('../controllers/userController');
 const authMiddleware = require('../middleware/authMiddleware');
 const { upload } = require('../utils/cloudinary');
 
+
 const router = express.Router();
 
 // حماية جميع المسارات التالية
 router.use(authMiddleware);
 router.get('/:id', userController.getUserById);
+// ... (في userRoutes.js)
+// --- ✅ أضف هذا المسار الجديد ---
+router.get('/me/details', userController.getMeDetails);
+
+
 
 // مسار لتحديث اسم المستخدم
 // Express سيبحث عن خاصية 'updateUsername' في الكائن الذي تم تصديره من userController
