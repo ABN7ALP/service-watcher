a0b9da1f99efa2cbee6f779e91223ff4d62588a8
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2025-12-30 05:50:49 +0300

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -29,22 +29,13 @@ const userSchema = new mongoose.Schema({
     isAdmin: { type: Boolean, default: false },
     socketId: { type: String, default: null },
 
-    // --- ✅ الحقول الجديدة لنظام الصداقة ---
-    friends: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    friendRequestsSent: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    friendRequestsReceived: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    // --- نهاية الحقول الجديدة --
+    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    friendRequestsSent: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    friendRequestsReceived: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    blockedUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
 
 }, { timestamps: true });
+
 
 userSchema.pre('validate', async function(next) {
     if (this.isNew && !this.customId) {
