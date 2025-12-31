04f6d5576434264fbd538889b5c0a35f916cba49
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2025-12-31 06:17:46 +0300

    Add route to check friendship status

--- server/routes/friendRoutes.js
+++ server/routes/friendRoutes.js
@@ -11,5 +11,6 @@ router.post('/send-request/:userId', friendController.sendFriendRequest);
 router.post('/accept-request/:userId', friendController.acceptFriendRequest);
 router.post('/reject-request/:userId', friendController.rejectOrCancelRequest);
 router.delete('/remove-friend/:userId', friendController.removeFriend);
+router.get('/check-status/:userId', friendController.checkFriendshipStatus);
 
 module.exports = router;
