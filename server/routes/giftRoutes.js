f4a545d2f0515e93131276330e2558e6f8d1f7d4
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2026-08-24 00:45:40 +0300

    Add giftRoutes for gift shop and leaderboard features

---
+++ server/routes/giftRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const giftController = require('../controllers/giftController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+router.use(authMiddleware);
+
+router.get('/shop', giftController.getGiftShop);
+router.post('/send', giftController.sendGift);
+router.get('/leaderboard/top-senders', giftController.getTopSendersThisMonth);
+router.get('/leaderboard/top-receivers', giftController.getTopReceiversThisMonth);
+
+module.exports = router;
