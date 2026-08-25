17697eddb0196d6075c9e77eac4271d32e31ef13
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2026-08-25 16:06:55 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.use('/api/blocks', require('./routes/blockRoutes')); // ✅ أضف هذا ا
 app.use('/api/private-chat', require('./routes/privateChatRoutes')); // ⭐ الجديد
 app.use('/api/chat-media', require('./routes/chatMediaRoutes')); // ⭐ الجديد
 app.use('/api/gifts', require('./routes/giftRoutes')); // ✅ نظام الهدايا والمتصدرين
-
+app.use('/api/coin-purchase', require('./routes/coinPurchaseRoutes')); // ✅ نظام شراء الكوينزات
 
 // --- مسار "التقاط الكل" لخدمة الواجهة الأمامية --
 app.get('*', (req, res) => {
