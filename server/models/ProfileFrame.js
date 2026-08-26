4f80c89315cca6ad4e8f99dfda027e0605edd1d2
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2026-08-27 01:20:33 +0300

    Update ProfileFrame.js

--- server/models/ProfileFrame.js
+++ server/models/ProfileFrame.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const profileFrameSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
-    price: { type: Number, required: true, min: 0 },
     cssClass: { type: String, required: true }, // كلاس CSS ثابت معرف بملف الأنماط
     previewImage: String,
     isActive: { type: Boolean, default: true },
