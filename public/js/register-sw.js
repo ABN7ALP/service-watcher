6f54845cc67745e088f13db07d6fc0545c24ea12
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2026-09-05 21:16:18 +0300

    Create register-sw.js

---
+++ public/js/register-sw.js
@@ -0,0 +1,5 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').catch(err => console.warn('SW registration failed:', err));
+    });
+}
