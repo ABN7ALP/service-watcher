7326c4c0e6e33522f6e9ff1716d89c1590ea293a
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2026-09-09 01:27:34 +0300

    Update withdrawalRoutes.js

--- server/routes/withdrawalRoutes.js
+++ server/routes/withdrawalRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const withdrawalController = require('../controllers/withdrawalController');
 const authMiddleware = require('../middleware/authMiddleware');
+const { financialLimiter } = require('../middleware/globalMiddleware');
 
 const router = express.Router();
 router.use(authMiddleware);
 
 router.get('/my-withdrawals', withdrawalController.getMyWithdrawals);
-router.post('/create', withdrawalController.createWithdrawal);
+router.post('/create', financialLimiter, withdrawalController.createWithdrawal);
 
 module.exports = router;
