2575bfd93c147aa47b9cef09597ac4fac5fc8435
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2026-09-09 01:27:57 +0300

    Update depositRoutes.js

--- server/routes/depositRoutes.js
+++ server/routes/depositRoutes.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const depositController = require('../controllers/depositController');
 const authMiddleware = require('../middleware/authMiddleware');
+const { financialLimiter } = require('../middleware/globalMiddleware');
 const { chatUpload } = require('../utils/cloudinary');
 
 router.use(authMiddleware);
 
 router.get('/wallet-info', depositController.getWalletInfo);
 router.get('/my-deposits', depositController.getMyDeposits);
-router.post('/create', depositController.createDepositRequest);
+router.post('/create', financialLimiter, depositController.createDepositRequest);
 router.post('/:depositId/receipt', chatUpload.single('file'), depositController.uploadDepositReceipt);
 
 module.exports = router;
