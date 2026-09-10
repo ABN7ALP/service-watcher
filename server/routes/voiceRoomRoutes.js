e0e0ac8e589720c99be61121cc3ad76e85b1861e
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2026-09-10 19:16:33 +0300

    Update voiceRoomRoutes.js

--- server/routes/voiceRoomRoutes.js
+++ server/routes/voiceRoomRoutes.js
@@ -21,6 +21,7 @@ router.get('/rooms', voiceRoomController.listRooms);
 router.get('/my-room', voiceRoomController.getMyRoom);
 router.post('/rooms', roomCreationLimiter, voiceRoomController.createRoom);
 router.get('/rooms/:id', voiceRoomController.getRoomById);
+router.get('/rooms/:id/messages', voiceRoomController.getRoomMessages);
 router.patch('/rooms/:id', voiceRoomController.updateRoom);
 
 router.get('/', voiceRoomController.getVoiceRoomState);
